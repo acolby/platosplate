94fafe4fb356090257183c830bce6443cde7e073
ac_airbear <acolby@live.com>
2015-02-20 00:57:43 -0800

    made it all work

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var express = require('express');
-var app = express();
+var Q = require('q');
+var Firebase = require('firebase');
+var HTTP = require('http');
+var spark = require('sparknode');
 
-var numberOfRequests = 0;
+var Firebase = require('firebase');
+var dataRef = new Firebase('https://platosplate.firebaseio.com/values');
+
  
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+var core = new spark.Core({
+  accessToken: 'd7e7917e35dfde39c5c9acabab3c13d8e72c702f',
+  id: '54ff6f066678574941510867'
 });
 
-app.get('/platosPlate/addDatappoint/:value', function(req, res, next) {
-  var value = req.params.value;
-  console.log(numberOfRequests++ + ' cool Beens! ' + value);
-  res.send('yoooo');
-});
+core.on('newWeight', function(info) {
+  console.log(info);
+  console.log(info.data);
 
-app.all('/*', function(req, res, next) {
-  console.log('Intercepting requests ...');
-  res.send('other requests'); 
-})
- 
-app.listen(3000);
+  dataRef.push(info);
+});
